01c5b5dfb08298cb1ac5abd0e8570c3b853d44d6
stamwoo <swstamwoo@gmail.com>
2019-02-03 20:35:41 +0100

    mod *.yml

--- config-server-git/src/main/java/me/stamwoo/configservergit/ConfigServerGitApplication.java
+++ config-server-git/src/main/java/me/stamwoo/configservergit/ConfigServerGitApplication.java
@@ -2,6 +2,7 @@ package me.stamwoo.configservergit;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
