16dc82499e38142e7df455acbdb96cdcfc2820e0
stamwoo <swstamwoo@gmail.com>
2019-02-08 15:43:33 +0100

    spring cloud gateway | filter

--- gateway/src/main/java/me/stamwoo/gateway/GatewayApplication.java
+++ gateway/src/main/java/me/stamwoo/gateway/GatewayApplication.java
@@ -1,7 +1,11 @@
 package me.stamwoo.gateway;
 
+import me.stamwoo.gateway.filter.ElapsedFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class GatewayApplication {
@@ -10,5 +14,21 @@ public class GatewayApplication {
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 
+
+	@Bean
+	public RouteLocator customerRouteLocator(RouteLocatorBuilder builder) {
+		// @formatter:off
+		return builder.routes()
+				.route(r -> r.path("/customer/**")
+						.filters(f -> f.stripPrefix(2)
+								.filter(new ElapsedFilter())
+								.addResponseHeader("X-Response-Default-Foo", "Default-Bar"))
+						.uri("lb://EUREKA-CONSUMER")
+						.order(0)
+						.id("service_customer")
+				)
+				.build();
+		// @formatter:on
+	}
 }
 
